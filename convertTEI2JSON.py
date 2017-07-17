1683f64d424247765a1da27e24d13d305f23bd4e
Deren Kudeki <dkudeki@illinois.edu>
2017-07-17 10:03:43 -0500

    Chronologies are now populated by a number of fields including date, but more fields need to be added

--- convertTEI2JSON.py
+++ convertTEI2JSON.py
@@ -1,4 +1,4 @@
-import os, json
+import os, json, datetime
 from shutil import copyfile
 from lxml import etree
 
@@ -6,6 +6,33 @@ if os.name == 'nt':
 	SLASH = '\\'
 else:
 	SLASH = '/'
+
+def readDate(target_date):
+	if len(target_date) == 4:
+		format = '%Y'
+	elif target_date[6:] == '00' and target_date[4:6] == '00':
+		format = '%Y0000'
+	elif target_date[6:] == '00':
+		format = '%Y%m00'
+	else:
+		format = '%Y%m%d'
+	return datetime.datetime.strptime(target_date,format)
+
+def generateCitation(bibl_root):
+	new_citation = {}
+	new_titles = bibl_root.xpath('.//title')
+	for title in new_titles:
+		new_types = title.xpath('./@type')
+		new_levels = title.xpath('./@level')
+		if ('es' in new_types or 're' in new_types) or 'a' in new_levels:
+			new_citation['headline'] = title.xpath('./text()')[0]
+			new_citation['@type'] = 'Text'
+		else:
+			new_citation['name'] = title.xpath('./text()')[0]
+			new_citation['@type'] = 'CreativeWork'
+
+
+	return new_citation
 
 def processTEIFile(tei_file):
 	with open(tei_file,'rb') as infile:
@@ -20,8 +47,34 @@ def processTEIFile(tei_file):
 			}
 		]
 	}
-	print(card)
+#	print(card)
 	root = etree.fromstring(card)
+
+	card_type = tei_file[tei_file.rfind('/')+1:][0]
+	if card_type == 's' or card_type == 'p':
+		#bibliography
+		pass
+	else:
+		#chronology
+		output_card['@id'] = root.xpath('/TEI/@xml:id')[0]
+		output_card['@type'] = 'Dataset'
+		output_card['author'] = { '@type': 'Person', '@id': 'http://viaf.org/viaf/44300868'}
+		output_card['inLanguage'] = 'fr'
+		output_card['temporalCoverage'] = readDate(root.xpath('//head/date/@value')[0]).date().isoformat()
+		
+		output_card['mentions'] = map((lambda x: { '@type': 'CreativeWork', 'title': x }),root.xpath('//div1//p/title/text()') + root.xpath('//div1//note/title/text()'))
+		if len(output_card['mentions']) == 1:
+			output_card['mentions'] = output_card['mentions'][0]
+		elif len(output_card['mentions']) == 0:
+			del output_card['mentions']
+
+#		print(tei_file,root.xpath('//div1//bibl'))
+		output_card['citation'] = map(generateCitation,root.xpath('//div1//bibl'))
+	#	if len(output_card['citation']) == 1:
+	#		output_card['citation'] = output_card['citation'][0]
+	#	elif len(output_card['citation']) == 0:
+	#		del output_card['citation']
+
 
 	return json.dumps(output_card,indent=4)
 
